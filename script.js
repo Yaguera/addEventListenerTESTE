98150e17b0ecaf11cbacab9a1cb16b74b794b909
Yago Gomes <yaguamineral@gmail.com>
2023-06-22 16:43:01 -0300

    add funçao para remover os epaços no inicio e fim

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const form = document.getElementById('form-name');
 /*função para validar se o nome vai ter mais de 3 palavras ( separa o nome em uma array onde o ' ' vai ser o separador, no caso Yago Gomes Varela --> ['Yago', 'Gomes', 'Varela'] que é igual a 3, entao seria valido*/
 const nomeBeneficiario = document.getElementById('nome-beneficiario');
+let formIsValid = false
     function validarNome(nomeCompleto) {
-        const nomeComoArray = nomeCompleto.split(' ');
+        const nomeSemEspaco = nomeCompleto.trim();
+        const nomeComoArray = nomeSemEspaco.split(' ');
         return nomeComoArray.length >= 3; /*vai retornar true ou false*/
     }
 /*Evento do tipo submit no form */ 
     form.addEventListener('submit', function(e) {
-        let formIsValid = false
         e.preventDefault();
 
         const numeroContaBeneficiario = document.getElementById('num-conta');
@@ -31,5 +32,15 @@ const nomeBeneficiario = document.getElementById('nome-beneficiario');
     })
 
 nomeBeneficiario.addEventListener('keyup', function(e){
-    console.log(nomeBeneficiario.value)
+    console.log(e.target.value);
+    formIsValid = validarNome(e.target.value)
+    if(!formIsValid) {
+        nomeBeneficiario.style.border = '1px solid red';
+        nomeBeneficiario.style.outlineColor = 'red';
+        const errorMensage = document.querySelector('.error-mensage').style.display = 'block';
+    } else {
+        nomeBeneficiario.style.border = "";
+        nomeBeneficiario.style.outlineColor = '';
+        const errorMensage = document.querySelector('.error-mensage').style.display = 'none';
+    }
 }) 
